ba25d11c54980e6a7c16c35170a1ac9abd320ce5
Queen Bajracharya <qbajracharya@gmail.com>
2026-05-28 02:16:02 +1000

    require lantern for attic entry

--- Door.cpp
+++ Door.cpp
@@ -1,4 +1,5 @@
 #include "Door.h"
+#include "Player.h"
 
 Door::Door(const std::string &n, const std::string &d, Room* from, Room* to, bool isLocked)
         : Passage(n, d, from, to), locked(isLocked) {}
@@ -20,5 +21,16 @@ void Door::enter() {
         std::cout << "The door is locked.\n\n";
         return;
     }
+
+    Player *player = Player::instance();
+    if (getTo()->getName() == "attic" && !player->getItem("lantern")) {
+        std::cout << "It is too dark upstairs to see.\n\n";
+        return;
+    }
+
     Passage::enter();
+
+    if (getTo()->getName() == "attic" && player->getItem("key") && player->getItem("lantern")) {
+        std::cout << "The future shall hold more quests for you.\n\n";
+    }
 }
